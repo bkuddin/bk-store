27453280905890005e60231ef555bbd9c32ab36b
BK Uddin <1bkhossain@gmail.com>
2021-09-13 06:12:24 +0600

    Defined total

--- js/app.js
+++ js/app.js
@@ -71,6 +71,8 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+
+  updateTotal();
 };
 
 //grandTotal update function
